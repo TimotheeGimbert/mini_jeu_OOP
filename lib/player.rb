019537b8a8ba772bbc9cce87d44c1f67c19c6ba7
TimotheeGimbert <timothee.gimbert@free.fr>
2021-10-19 00:42:55 +0200

    adds HumanPlayer class

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,4 @@
 class Player
-  
   attr_accessor :name, :life_points
 
   def initialize(name)
@@ -10,12 +9,10 @@ class Player
   def show_state
     puts "#{@name} a #{@life_points} points de vie"
   end
-
   def gets_damage(damages)
     @life_points -= damages
     if @life_points <= 0 then puts "Le joueur #{@name} a été tué !" end
   end
-
   def attacks(player)
     puts "Le joueur #{@name} attaque le joueur #{player.name} ... !"
     damages = compute_damage
@@ -24,9 +21,24 @@ class Player
     player.gets_damage(damages)
     sleep 0.5
   end
-
   def compute_damage
     return rand(1..6)
+  end
+
+end
+
+  
+class HumanPlayer < Player
+  attr_accessor :weapon_level
+
+  def initialize(name)
+    super(name)
+    @life_points = 100
+    @weapon_level = 1
+  end
+
+  def show_state
+    puts "#{@name} a #{@life_points} et une arme de niveau #{@weapon_level}"
   end
 
 end
